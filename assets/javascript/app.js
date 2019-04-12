71343dadb3136b6d57e5b8edcc099b8fd3e9899d
Randall <randall@Randalls-MBP.attlocal.net>
2019-04-12 15:34:35 -0700

    changed JS variables to an overarching array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,96 @@
 //VARIABLES
 var correct =   0;       //displays number correct
 var incorrect = 0;       //displays incorrect
-var guess =     "";      //will hold the user's guess
 var questions;           //questions array
-var possibleAnswers;     //will hold the array of answer options
-var correctAnswers;      //correct Answer with letter designation
-var timeRemaining;               //time left
+var timeRemaining = 120;               //time left
 
 //QUESTIONS AND ANSWERS DEFINED
-questions = [
-    "1. Which of these is NOT a name of the Stark children's direwolves?",
-    "2. Which of these is the name given to the bastards of Dorne?",
-    "3. Who is king of Westeros when the the series begins?",
-    "4 .what must an Unsullied kill to prove he has no mercy or weakness?",
-    "5. Which of the following types of wine does the wine merchant attempt to poison Daenerys with?",
-    "6. What is the name of Arya Stark's sword?",
-    "7. What house has a sigil of a lion?",
-    "8. By what name do the Seven Kingdoms refer to the Free Folk who live in north beyond the Wall?",
-    "9. How many men did Theon Greyjoy use to capture Winterfell?",
-    "10. Who is Jon Snow's mother?",
-];
-// console.log(questions[0]);
+var questions = [
+    {
+    question: "1. Which of these is NOT a name of the Stark children's direwolves?",
+    answer: [" Nymeria", " Ghost", " Lightning"],
+    correct: 2,
+}, {
+    question: "2. Which of these is the name given to the bastards of Dorne?",
+    answer: ["Sand", " Pyke", " Snow"],
+    correct: 0,
+}, {
+    question: "3. Who is king of Westeros when the the series begins?",
+    answer: ["Aegon Targaryan", " Robert Baratheon", " Tywin Lannister"],
+    correct: 1,
+}, {
+    question: "4 .what must an Unsullied kill to prove he has no mercy or weakness?",
+    answer: ["A White-Walker", " A dragon", " A newborn slave child"],
+    correct: "2",
+}, {
+    question: "5. Which of the following types of wine does the wine merchant attempt to poison Daenerys with?",
+    answer: ["Stormland Red", " Arbor Red", " Merlot"],
+    correct: "1",
+}, {
+    question: "6. What is the name of Arya Stark's sword?",
+    answer: ["Needle", " Pincer", " Valyrian"],
+    correct: "0",
+}, {
+    question: "7. What house has a sigil of a lion?",
+    answer:  ["House Lannister", " House Tully", "House Fray"],
+    correct: "0",
+}, {
+    question: "8. By what name do the Seven Kingdoms refer to the Free Folk who live in north beyond the Wall?",
+    answer: ["Andals", " Wildlings", " Crow"],
+    correct: "1",
+}, {
+    question: "9. How many men did Theon Greyjoy use to capture Winterfell?",
+    answer: ["2000", " 200", " 20"],
+    correct: "2",
+}, {
+    question: "10. Who is Jon Snow's mother?",
+    answer: ["Lyanna Stark", " Lyanna Mormont", " Caitlyn Stark"],
+    correct: "0",
+}];
 
-possibleAnswers = [
-    [" Nymeria", " Ghost", " Lightning"],
-    ["Sand", " Pyke", " Snow"],
-    ["Aegon Targaryan", " Robert Baratheon", " Tywin Lannister"],
-    ["A White-Walker", " A dragon", " A newborn slave child"],
-    ["Stormland Red", " Arbor Red", " Merlot"],
-    ["Needle", " Pincer", " Valyrian"],
-    ["House Lannister", " House Tully", "House Fray"],
-    ["Andals", " Wildlings", " Crow"],
-    ["2000", " 200", " 20"],
-    ["Lyanna Stark", " Lyanna Mormont", " Caitlyn Stark"]
-];
-// console.log(answers[0][1]);
 
-//dynamically create a radio button before each possibleAnswer
+function displayQA(){
+    
+    var questionHolder = $("#questions").text(questions);       //display questions in div #questions
+    var answerHolder =  $("#answers").text(questions.answer);
+    for (var i = 0; i < questions.length; i++) {                  //looops through each question
+        questionHolder.html(questions.question[i]); //append eash question to a new div
 
-correctAnswers  = [
-    "C. Lightning",
-    "A. Sand",
-    "B. Robert Baratheon",
-    "C. A newborn slave child",
-    "B. Arbor Red",
-    "A. Needle",
-    "A. House Lannister",
-    "B. Wildlings",
-    "C. 20",
-    "A. Lyanna Stark"
-]
-// console.log(correctAnswers.length);
+    //create varibales to hold possible answers
+        var answer1 = questions[i].answer[0];
+        var answer2 = questions[i].answer[1];
+        var answer3 = questions[i].answer[2];
 
-//create a variable that adds each question to the div id="questions"
-var questionHolder = $("#questions").text(questions);
-// console.log(questionHolder);
+    //reate radio buttons for each possible questions.answer using possible answers
+        answerHolder.append(answer1);
+        answerHolder.append(answer2);
+        answerHolder.append(answer3);
+    }
+};
 
-//create a variable that turns each answers into clickable options
-var answerHolder =  $("#answers").text(possibleAnswers);       //appends available answers to answers div
 
-var resultsHolder = $("#results").html("<h3>Your score:</h3>Correct: " + correct +"<br>Incorrect: "+ incorrect);
+// //create a variable that adds each question to the div id="questions"
+// var questionHolder = $("#questions").text(questions);
+// // console.log(questionHolder);
 
-//create radio buttons for possibleAnswers
+// //create a variable that turns each answers into clickable options
+// var answerHolder =  $("#answers").text(possibleAnswers);       //appends available answers to answers div
 
-// var gameTimer ={
+// var resultsHolder = $("#results").html("<h3>Your score:</h3>Correct: " + correct +"<br>Incorrect: "+ incorrect);
 
-//     timeRemaining = 120,    //seconds remaining in timer
+// //create radio buttons for possibleAnswers
 
-//     //function to set time
-//     function() {
-//         $("#time").text("Time remaining: " + gameTimer.timeRemaining);
-//         setInterval(gameTimer.countdown, 1000);
-//         $("#time").hide();      //hide until clicked
-//     },
+
+// function setTime() {
+//     $("#time").text("Time remaining: " + timeRemaining +" seconds");
+//     setInterval(timeRemaining.countdown, 1000);
+// };
 
 //     //function to decrement time
-//     function() {
-//         gameTimer.timeRemaining--;
-//     }
-// }
+// function timeDecr() {
+//     gameTimer.timeRemaining--;
+//     $("#time").text("Time remaining: " + timeRemaining +" seconds");
+// };
 
 //hide divs until click event
 $(function(){
@@ -93,35 +105,38 @@ $(function(){
 $(function(){
     $("#results").hide();
 })
-//create radio btns that are places before each index in "answers"
+// //create radio btns that are places before each index in "answers"
 
 //create an if/else statement to check answer
-if (possibleAnswers[0] == correctAnswers[0]) {
+if (questions[i].answer[i] == questions[i].correct[i]) {
     correct++;          //if answer is correct
     } else {
     incorrect++         //else answer is incorrect
 }
 
-//create a variable that adds the corresponding set of answers to the div id="answers"
+// //create a variable that adds the corresponding set of answers to the div id="answers"
 
-// FUNCTIONS
+// // FUNCTIONS
 
-//GAME START
+// GAME START
 $(document).ready(function(){           //when page loads
     $("#start").click(function(){       //when start button is clicked
         $("#start").hide();             //hide the "Start" button
-        $("#time").show();
-        $("#questions").show();         //display the questions
-        $("#answers").show();
-        $("#submit").show();            //display dynamically created submit button
+        displayQA();
+//         $("#time").show();
+//         $("#questions").show();         //display the questions
+//         $("#answers").show();
+//         $("#submit").show();            //display dynamically created submit button
+//         displayQA();
+//         setTime();
+//         timeDecr();
     })
 
-    $("#submit").click(function(){      //when submit is clicked
-        $("#questions").hide();
-        $("#submit").hide();
-        $("#answers").hide();
-        $("#results").show();           //display results
-    })
-});
-
+//     $("#submit").click(function(){      //when submit is clicked
+//         $("#questions").hide();
+//         $("#submit").hide();
+//         $("#answers").hide();
+//         $("#results").show();           //display results
+//     })
+})
 
